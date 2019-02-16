14e29fa441b69e9fb2bf462e528f9deea1468fa2
Swati Kanchan <swatikanchan707@gmail.com>
2019-02-17 02:01:09 +0530

    Layout enhanced

--- demos/gridGroupsLayout.py
+++ demos/gridGroupsLayout.py
@@ -6,9 +6,16 @@ Created on Sat Feb 16 11:22:31 2019
 """
 
 import sys
+from PyQt5 import QtGui
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication, QCheckBox, QGridLayout, QGroupBox,
         QMenu, QPushButton, QRadioButton, QVBoxLayout, QWidget)
+
+import pyqtgraph as pg
+import numpy as np
+
+from pyqtgraph.Qt import QtCore
+from medpy.io import load
 
 class Window(QWidget):
     def __init__(self, parent=None):
@@ -16,31 +23,82 @@ class Window(QWidget):
 
         grid = QGridLayout()
         grid.addWidget(self.createExampleGroup(), 0, 0)
-        grid.addWidget(self.createExampleGroup(), 1, 0)
-        grid.addWidget(self.createExampleGroup(), 0, 1)
-        grid.addWidget(self.createExampleGroup(), 1, 1)
+        grid.addWidget(self.createExampleGroup1(), 0, 1)
+        grid.addWidget(self.createExampleGroup1(), 1, 0)
+        grid.addWidget(self.createExampleGroup1(), 1, 1)
         self.setLayout(grid)
 
         self.setWindowTitle("PyQt5 Group Box")
         self.resize(400, 300)
 
     def createExampleGroup(self):
-        groupBox = QGroupBox("Best Food")
+        groupBox = QGroupBox("Controls")
 
-        radio1 = QRadioButton("&Radio pizza")
-        radio2 = QRadioButton("R&adio taco")
-        radio3 = QRadioButton("Ra&dio burrito")
+        vBoxLayout = QVBoxLayout()
 
-        radio1.setChecked(True)
+        #add buttons for part 1
+        button1=QPushButton("T1",self)
+        vBoxLayout.addWidget(button1)
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(radio1)
-        vbox.addWidget(radio2)
-        vbox.addWidget(radio3)
-        vbox.addStretch(1)
-        groupBox.setLayout(vbox)
+        button2=QPushButton("T2",self)
+        vBoxLayout.addWidget(button2)
+        
+        button3=QPushButton("TC",self)
+        vBoxLayout.addWidget(button3)
+
+        button4=QPushButton("F",self)
+        vBoxLayout.addWidget(button4)
+
+        button5=QPushButton("SEGMENT",self)
+        vBoxLayout.addWidget(button5)
+#        button5.clicked.connect(self.func)
+
+        button=QPushButton("CLOSE",self)                
+        vBoxLayout.addWidget(button)
+
+        vBoxLayout.addStretch(1)
+        groupBox.setLayout(vBoxLayout)
 
         return groupBox
+    
+    def createExampleGroup1(self):
+        groupBox = QGroupBox("3D View")
+
+        self.pgcustom = imagePlot()
+        vBoxLayout = QVBoxLayout()
+        vBoxLayout.addWidget(self.pgcustom.imv)
+#        vBoxLayout.addStretch(1)
+        groupBox.setLayout(vBoxLayout)
+
+        return groupBox
+    
+class imagePlot(pg.ImageView):
+    
+    def __init__(self):
+        dp_input, image_header = load('DP_preprocessed.nii.gz')
+        data = np.asarray(dp_input)
+    
+        # Interpret image data as row-major instead of col-major
+        pg.setConfigOptions(imageAxisOrder='col-major')
+        self.imv = pg.ImageView()
+    
+        ## Display the data and assign each frame a time value from 1.0 to 3.0
+        self.imv.setImage(data, xvals=np.linspace(1, 144, data.shape[0], dtype = 'int32'))
+        ## Set a custom color map
+        colors = [
+            (0, 0, 0),
+            (45, 5, 61),
+            (84, 42, 55),
+            (150, 87, 60),
+            (208, 171, 141),
+            (255, 255, 255)
+        ]
+        self.cmap = pg.ColorMap(pos=np.linspace(0.0, 1.0, 6), color=colors)
+        self.imv.setColorMap(self.cmap)
+        self.imv.setCurrentIndex(72)
+        self.imv.ui.roiBtn.hide()
+        self.imv.ui.menuBtn.hide()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
---
+++ stage4LayoutComplete.py
@@ -0,0 +1,350 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 16 23:49:25 2019
+
+@author: swati
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 14 17:19:37 2019
+
+@author: swati
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 12 11:00:15 2019
+
+@author: swati
+"""
+
+from PyQt5.QtCore import QFile, QFileInfo, QSettings, Qt
+from PyQt5 import QtGui
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (QApplication, QMainWindow,QWidget, QAction, QMessageBox, QLabel, QRadioButton, 
+    QFileDialog, QGridLayout, QPushButton, QMenu, QGroupBox, QVBoxLayout, QHBoxLayout, QScrollArea)
+import sys
+
+import pyqtgraph as pg
+import numpy as np
+
+from pyqtgraph.Qt import QtCore
+from medpy.io import load
+
+
+class Window(QMainWindow):
+    MaxRecentFiles = 5
+        
+    def __init__(self):
+        super().__init__()
+        
+        self.recentFileActs = []
+        self.title = "QMenuBar"
+        self.top = 200
+        self.left = 200
+        self.width = 800
+        self.height = 800
+        self.setWindowIcon(QtGui.QIcon("images/saveSegmentedMRI.jpg"))
+        self.widget = Layout(parent=self)
+        self.setCentralWidget(self.widget)
+        
+        self.createActions()
+        self.createMenus()
+        self.statusBar()
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+
+    def createActions(self):
+        self.openAct = QAction(QIcon("Images/open.png"), 'Open', self, shortcut="Ctrl+O", statusTip="Open MRI (.nii, .nii.gz, .mha)", triggered=self.OpenMRI)
+        self.openLastAct = QAction(QIcon("Images/open.png"), 'Open last closed', self, shortcut="Ctrl+Shift+T", statusTip="Open last closed MRI (.nii, .nii.gz, .mha)", triggered=self.OpenLastMRI)
+        for i in range(Window.MaxRecentFiles):
+            self.recentFileActs.append(
+                    QAction(self, visible=False,
+                            triggered=self.OpenRecentMRI))
+        self.saveSegAct = QAction(QIcon("Images/saveSegmentedMRI.png"), 'Save segmented MRI', self, shortcut="Ctrl+S", statusTip="Save segmented MRIs", triggered=self.SaveSegmentedMRI)
+        self.saveMaskAct = QAction(QIcon("Images/saveMask.png"), 'Save mask', self, shortcut="Ctrl+Shift+M", statusTip="Save Mask of segmented MRIs", triggered=self.SaveMask)
+        self.exitAct = QAction(QIcon("Images/close.png"), 'Exit', self, shortcut="Ctrl+Q", statusTip="Exit Application", triggered=self.CloseApp)
+        self.aboutAct = QAction(QIcon("Images/about.png"), 'About', self, shortcut="Ctrl+A", statusTip="About software", triggered=self.AboutSoftware)
+        self.tutorialAct = QAction(QIcon("Images/tutorial.png"), 'Tutorial', self, statusTip="Demo Tutorial", triggered=self.AboutSoftware)
+
+        
+    def createMenus(self):
+        
+        self.mainMenu = self.menuBar()
+        self.fileMenu = self.mainMenu.addMenu("File")     
+        self.fileMenu.addAction(self.openAct)
+        self.fileMenu.addAction(self.openLastAct)
+        
+        self.openRecentMenu = QMenu('Open Recent', self)
+        self.separatorAct = self.openRecentMenu.addSeparator()
+        for i in range(Window.MaxRecentFiles):
+            self.openRecentMenu.addAction(self.recentFileActs[i])
+        self.fileMenu.addMenu(self.openRecentMenu)
+        self.fileMenu.addSeparator()
+        self.updateRecentFileActions()
+
+        
+        self.fileMenu.addAction(self.saveSegAct)
+        self.fileMenu.addAction(self.saveMaskAct)
+        self.fileMenu.addAction(self.exitAct)
+        self.menuBar().addSeparator()
+        self.helpMenu = self.mainMenu.addMenu("Help")
+        self.helpMenu.addAction(self.aboutAct)
+        self.helpMenu.addAction(self.tutorialAct)
+
+    def CloseApp(self):
+        reply = QMessageBox.question(self, "Close Message", "Are You Sure To Close Window",
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            self.close()
+            
+    def OpenMRI(self):
+        print("In open")
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;NIfTI -1 (*.nii;*.nii.gz;*.mha)")
+        if fileName:
+            print(fileName)
+            self.loadFile(fileName)
+        
+    def OpenLastMRI(self):
+        print("In open last MRI")
+        settings = QSettings('Trolltech', 'Recent Files Example')
+        files = settings.value('recentFileList', [])
+        if(files[0]):
+            fileName = files[0]
+            print(fileName)
+            self.loadFile(fileName)
+        
+    def OpenRecentMRI(self):
+        print("In open recent MRI")
+        action = self.sender()
+        if action:
+            fileName = action.data()
+            self.loadFile(fileName)
+
+    def SaveSegmentedMRI(self):
+        print("In SaveSegmentedMRI") 
+        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","All Files (*);;NIfTI -1 (*.nii;*.nii.gz;*.mha);;NumPy array(*.npy;*.npz);;Images JPEG(*.jpg,*.png,*.jpeg)")
+        if fileName:
+            print(fileName)
+            self.saveFile(fileName)
+
+    def SaveMask(self):
+        print("In save mask")
+        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","All Files (*);;NIfTI -1 (*.nii;*.nii.gz;*.mha);;NumPy array(*.npy;*.npz);;Images JPEG(*.jpg,*.png,*.jpeg)")
+        if fileName:
+            print(fileName)
+            self.saveFile(fileName)
+        
+    def AboutSoftware(self):
+        print("In AboutSoftware") 
+        QMessageBox.about(self, "About Recent Files",
+                "The <b>Recent Files</b> example demonstrates how to provide "
+                "a recently used file menu in a Qt application.")
+        
+    def loadFile(self, fileName):
+        print("Loading...", fileName)
+        self.setCurrentFile(fileName)
+        self.statusBar().showMessage("File loaded", 2000)
+    
+    def saveFile(self, fileName):
+        print("Saving...", fileName)
+        self.setCurrentFile(fileName)
+        self.statusBar().showMessage("File saved", 2000)
+        
+    def setCurrentFile(self, fileName):
+        self.curFile = fileName
+        if self.curFile:
+            self.setWindowTitle("%s - Recent Files" % self.strippedName(self.curFile))
+        else:
+            self.setWindowTitle("Recent Files")
+
+        settings = QSettings('Trolltech', 'Recent Files Example')
+        files = settings.value('recentFileList', [])
+
+        try:
+            files.remove(fileName)
+        except ValueError:
+            pass
+
+        files.insert(0, fileName)
+        del files[Window.MaxRecentFiles:]
+
+        settings.setValue('recentFileList', files)
+
+        for widget in QApplication.topLevelWidgets():
+            if isinstance(widget, Window):
+                widget.updateRecentFileActions()
+
+    def updateRecentFileActions(self):
+        settings = QSettings('Trolltech', 'Recent Files Example')
+        files = settings.value('recentFileList', [])
+
+        numRecentFiles = min(len(files), Window.MaxRecentFiles)
+
+        for i in range(numRecentFiles):
+            text = "&%d %s" % (i + 1, self.strippedName(files[i]))
+            self.recentFileActs[i].setText(text)
+            self.recentFileActs[i].setData(files[i])
+            self.recentFileActs[i].setVisible(True)
+
+        for j in range(numRecentFiles, Window.MaxRecentFiles):
+            self.recentFileActs[j].setVisible(False)
+
+        self.separatorAct.setVisible((numRecentFiles > 0))
+
+    def strippedName(self, fullFileName):
+        return QFileInfo(fullFileName).fileName()
+        
+class Layout(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        grid = QGridLayout()
+        self.controlLayout = self.createControlLayout()
+        self.mriView = self.createMRIView()
+        self.maskView = self.createMaskView()
+        self.segmentedView = self.createSegmentedView()
+        grid.addWidget(self.controlLayout, 0, 0)
+        grid.addWidget(self.mriView, 0, 1)
+        grid.addWidget(self.maskView, 1, 0)
+        grid.addWidget(self.segmentedView, 1, 1)
+        
+#        self.maskView.setHidden(True)
+#        self.segmentedView.setHidden(True)
+        
+        scroll = QScrollArea()
+        scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        scroll.setWidgetResizable(False)
+        scroll.setLayout(grid)
+        
+        vLayout = QVBoxLayout(self)
+        vLayout.addWidget(scroll)
+        self.setLayout(vLayout)
+        
+#        self.setLayout(grid)
+        
+    def toggleResultView(self):
+        self.maskView.setHidden(not self.maskView.isHidden())
+        self.segmentedView.setHidden(not self.segmentedView.isHidden())
+        
+    def createControlLayout(self):
+        groupBox = QGroupBox("Controls")
+
+        vBoxLayout = QVBoxLayout()
+        
+        hboxModality = QHBoxLayout()
+        modalityLabel = QLabel("Choose Modality:")
+        hboxModality.addWidget(modalityLabel)
+        
+        t1Btn = QRadioButton("T1",self)
+        hboxModality.addWidget(t1Btn)
+
+        t2Btn = QRadioButton("T2",self)
+        hboxModality.addWidget(t2Btn)
+        
+        t1cBtn = QRadioButton("T1c",self)
+        hboxModality.addWidget(t1cBtn)
+
+        flairBtn = QRadioButton("FLAIR",self)
+        hboxModality.addWidget(flairBtn)
+        t1Btn.setChecked(True)
+        
+        vBoxLayout.addLayout(hboxModality)
+
+        hbox = QHBoxLayout()
+        segmentLabel = QLabel("Segment:")
+        hbox.addWidget(segmentLabel)
+        comboBox = QtGui.QComboBox(self)
+        comboBox.addItem("-Select-")
+        comboBox.addItem("Tumor")
+        comboBox.addItem("Cerebrospinal Fluid (CSF)")
+        comboBox.addItem("Gray Matter (GM)")
+        comboBox.addItem("White Matter (WM)")
+        comboBox.activated[str].connect(self.style_choice)
+        hbox.addWidget(comboBox)
+        vBoxLayout.addLayout(hbox)
+
+        groupBox.setLayout(vBoxLayout)
+
+        return groupBox
+    
+    def createMRIView(self):
+        groupBox = QGroupBox("MRI View")
+
+        self.pgcustom = imagePlot(fileName = 'DP_preprocessed.nii.gz')
+        vBoxLayout = QVBoxLayout()
+        vBoxLayout.addWidget(self.pgcustom.imv)
+        saveBtn = QPushButton("Save",self)
+        vBoxLayout.addWidget(saveBtn)
+#        vBoxLayout.addStretch(1)
+        groupBox.setLayout(vBoxLayout)
+
+        return groupBox
+    
+    def createMaskView(self):
+        groupBox = QGroupBox("Mask View")
+
+        self.pgcustom = imagePlot(fileName = 'DP_preprocessed.nii.gz')
+        vBoxLayout = QVBoxLayout()
+        vBoxLayout.addWidget(self.pgcustom.imv)
+        saveBtn = QPushButton("Save",self)
+        vBoxLayout.addWidget(saveBtn)
+#        vBoxLayout.addStretch(1)
+        groupBox.setLayout(vBoxLayout)
+
+        return groupBox
+    
+    def createSegmentedView(self):
+        groupBox = QGroupBox("Segmentation View")
+
+        self.pgcustom = imagePlot(fileName = 'DP_preprocessed.nii.gz')
+        vBoxLayout = QVBoxLayout()
+        vBoxLayout.addWidget(self.pgcustom.imv)
+        saveBtn = QPushButton("Save",self)
+        vBoxLayout.addWidget(saveBtn)
+#        vBoxLayout.addStretch(1)
+        groupBox.setLayout(vBoxLayout)
+
+        return groupBox
+    
+    def style_choice(self, text):
+        print(text)
+    
+    
+class imagePlot(pg.ImageView):
+    
+    def __init__(self, fileName):
+        dp_input, image_header = load(fileName)
+        data = np.asarray(dp_input)
+    
+        # Interpret image data as row-major instead of col-major
+        pg.setConfigOptions(imageAxisOrder='col-major')
+        self.imv = pg.ImageView()
+    
+        ## Display the data and assign each frame a time value from 1.0 to 3.0
+        self.imv.setImage(data, xvals=np.linspace(1, 144, data.shape[0], dtype = 'int32'), scale=(120,120))
+        ## Set a custom color map
+        colors = [
+            (0, 0, 0),
+            (45, 5, 61),
+            (84, 42, 55),
+            (150, 87, 60),
+            (208, 171, 141),
+            (255, 255, 255)
+        ]
+        self.cmap = pg.ColorMap(pos=np.linspace(0.0, 1.0, 6), color=colors)
+        self.imv.setColorMap(self.cmap)
+        self.imv.setCurrentIndex(72)
+        self.imv.ui.roiBtn.hide()
+        self.imv.ui.menuBtn.hide()
+        
+#        'xMin', 'xMax', 'yMin', 'yMax', 'minXRange', 'maxXRange', 'minYRange', 'maxYRange'
+#        self.setLimits(())
+
+        
+App = QApplication(sys.argv)
+window = Window()
+sys.exit(App.exec())
